7bcba6839590288a41fc368ad5ddeff595642c59
Christopher Smith <Crizzly@Christophers-MacBook-Air.local>
2017-11-08 20:58:01 -0800

    Added tableview and set up connections between the delegate and datasource

---
+++ Contacts/Controllers/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by Christopher Smith on 11/8/17.
+//  Copyright © 2017 Christopher Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+    }
+}
