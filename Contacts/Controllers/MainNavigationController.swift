e1c5990ce340ec4d492abd46b8f23099e43b8dec
Christopher Smith <Crizzly@Christophers-MacBook-Air.local>
2017-11-10 16:26:01 -0800

    Added navigation controller and altered constraints - Added MainNavigationController for easier navigation - Altered the contraints of labels on ContactDetailViewController - Changed background view color of ContactDetailViewController to cyan

---
+++ Contacts/Controllers/MainNavigationController.swift
@@ -0,0 +1,15 @@
+//
+//  MainNavigationController.swift
+//  Contacts
+//
+//  Created by Christopher Smith on 11/10/17.
+//  Copyright © 2017 Christopher Smith. All rights reserved.
+//
+
+import UIKit
+
+class MainNavigationController: UINavigationController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
